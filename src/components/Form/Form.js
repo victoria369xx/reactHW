79a6efdf945eb82caceab59fe465bf6a4fb55bbe
Victoria <v.shurkalina@gmail.com>
2022-02-28 20:52:42 +0400

    edited, author is const

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,8 +8,8 @@ import SendIcon from '@mui/icons-material/Send';
 
 export function Form (props){
     const [text, setMsgText] = useState('');
-    const [author, setMsgAuthor] = useState('');
     let [msg, setMsg] = useState({}); 
+    const author = 'User';
 
     const inputRef = useRef(null);
 
@@ -18,7 +18,6 @@ export function Form (props){
       });
 
       function clearForm () {
-          setMsgAuthor(" ");
           setMsgText(" ")
       }
 
@@ -35,8 +34,7 @@ export function Form (props){
             justifyContent: 'center'
         }}>
         <form onSubmit={submitHandler}>
-        <Input  required inputRef={inputRef}  id="outlined-required"  label="Author" size="small"  defaultValue={author} onChange={event => setMsgAuthor(event.target.value)} sx={{mr:1}}/>
-        <Input  required  id="outlined-required"  label="Text" size="small"  defaultValue={text} onChange={event => setMsgText(event.target.value)} sx={{mr:1}}/>
+        <Input  required  inputRef={inputRef} id="outlined-required"  label="Enter your message here" size="small"  value={text} onChange={event => setMsgText(event.target.value)} sx={{mr:1}}/>
             <Button type='submit' variant="contained" size="medium" sx={{p:1}}> <SendIcon/> </Button>
         </form>
         </Box>
